d336bf3dbc97109f7b05ef2fbea11e6e425f9d26
Donal Laptop <d.l.odonoghue@gmail.com>
2015-05-26 22:28:51 +0100

    counting unique volunteers placed, not unique placements

--- src/PL2_gender.r
+++ src/PL2_gender.r
@@ -20,16 +20,16 @@ placements_and_vols <- merge(x = placements, y = volunteers,
                              by = "Volunteer_ID", all.x = TRUE)
 
 # total volunteers placed by Gender and Year ----------------------------------
-placements_by_gender    <- placements_and_vols %>%
+vols_placed_by_gender    <- placements_and_vols %>%
                     group_by(Placement_Date_Year, Gender) %>%
-                    summarise(Num_placements = n_distinct(Volunteer_ID)) %>%
+                    summarise(Num_vols_placed = n_distinct(Volunteer_ID)) %>%
                     ungroup()                     
 
-View(placements_by_gender)
+View(vols_placed_by_gender)
 
 # sanity check; is total volunteers placed in 2014 equal to 4555? -------------
-df <- placements_by_gender[placements_by_gender$Placement_Date_Year == "2014",]
-ifelse(sum(df$Num_placements) == 4555,
+df <- vols_placed_by_gender[vols_placed_by_gender$Placement_Date_Year == "2014",]
+ifelse(sum(df$Num_vols_placed) == 4555,
        "correct total volunteers placed in 2014",
        "total number of volunteers placed in 2014 is incorrect")
 
