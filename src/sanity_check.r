c3d115d1330e9d3264fcde5d8433f38efcdc9452
Motasim <MotaSay@gmail.com>
2015-05-31 18:04:43 +0100

    Also get the numbers for when the number of Vol required < 21

--- src/sanity_check.r
+++ src/sanity_check.r
@@ -29,3 +29,35 @@ total_placements <- placements %>%
                     summarise(Num_placements = n_distinct(Placement_ID)) %>%
                     ungroup()                     
 print(total_placements)
+
+# same stats but for Number.of.Volunteers.Required < 21
+
+# total opportunity applications 2014
+total_applications <- merge(opps_applied_for, opportunities[, c("Opportunity_ID",
+                                                                "Number.of.Volunteers.Required")],
+                            by = "Opportunity_ID", all.x = TRUE) %>%
+  filter(Number.of.Volunteers.Required < 21) %>%
+  group_by(Application_Date_Year) %>%
+  summarise(Num_Apps = n_distinct(Application_ID)) %>%
+  ungroup()
+print(total_applications)
+
+# total volunteers placed 2014 ------------------------------------------------
+total_volunteers_placed <- merge(placements, opportunities[, c("Opportunity_ID",
+                                                               "Number.of.Volunteers.Required")],
+                                 by = "Opportunity_ID", all.x = TRUE) %>%
+  filter(Number.of.Volunteers.Required < 21) %>%
+  group_by(Placement_Date_Year) %>%
+  summarise(Num_vols_placed = n_distinct(Volunteer_ID)) %>%
+  ungroup()
+print(total_volunteers_placed)
+
+# total placements in 2014 ----------------------------------------------------
+total_placements <- merge(placements, opportunities[, c("Opportunity_ID",
+                                                        "Number.of.Volunteers.Required")],
+                          by = "Opportunity_ID", all.x = TRUE) %>%
+  filter(Number.of.Volunteers.Required < 21) %>%
+  group_by(Placement_Date_Year) %>%
+  summarise(Num_placements = n_distinct(Placement_ID)) %>%
+  ungroup()                     
+print(total_placements)
