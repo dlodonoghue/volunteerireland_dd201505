ea2dd2428e9a04d8706e6936acf2cb53b4b91096
papageorgiou <alpapag@gmail.com>
2015-06-07 09:20:54 +0100

    initial commit for AP6	Count Applications for 2014, by level of English reqd

--- src/AP6_eng_level.R
+++ src/AP6_eng_level.R
@@ -31,11 +31,13 @@ sum(is.na(opportunities$Level.of.English.required.for.role)) / nrow(opportunitie
 appscount_2014_by_eng_level <- applications_opps %>%
     filter(Application_Date_Year == "2014") %>%
     group_by(Level.of.English.required.for.role) %>%
-    summarise(Num_Applications = n_distinct(Application_ID)) %>%
+    summarise(Num_Applications = n_distinct(Application_ID)) 
+
+appscount_2014_with_ratio <- appscount_2014_by_eng_level %>%
     mutate (ratio=100*Num_Applications/sum(appscount_2014_by_eng_level$Num_Applications))
 
 
-View(appscount_2014_by_eng_level)
+View(appscount_2014_with_ratio)
 
-
+ 
 
